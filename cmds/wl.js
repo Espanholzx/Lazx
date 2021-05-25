dad191ed2897c51905b0bd0f2b78f0cead75e718
Espanholzx <77551376+Espanholzx@users.noreply.github.com>
2021-05-25 12:02:03 -0300

    Update wl.js

--- cmds/wl.js
+++ cmds/wl.js
@@ -1,26 +1,26 @@
 const Discord = require("discord.js")
 const mysql = require("mysql")
-
+const {name, ip, db, password} = require('./db.json');
 module.exports = {
     name: "wl",
     description: "wl",
     aliases: ["wl"],
     async execute(bot,message,args) {
         let con = mysql.createConnection({
-            host: "127.0.0.1", // IP DA DB
-            user: "root", // USER DA DB
-            password: "", // SENHA DA DB
-            database: "vrp" // NOME DA DB
+            host: ip,
+            user: name,
+            password: password,
+            database: db,
         })
 
         let id = args[0]
-        if(!id) return message.reply("Você não colocou Um ID") // VAI RETORNAR SE NÃO BOTAR STEAM HEX
+        if(!id) return message.reply("Você não colocou a steam hex")
         let valor = 1
-        let sql = `UPDATE vrp_infos SET whitelist = '${valor}' WHERE user_id = '${id}'` // MUDA DE DB PARA DB
+        let sql = `UPDATE vrp_infos SET whitelist = '${valor}' WHERE steam = '${id}'`
         con.query(sql, function (err, result) {
             if (err) throw err;
             let embed = new Discord.MessageEmbed()
-            .setDescription(`<a:verified:845321565339516978> |O ID **${id}** Teve a Whitelist aprovada.`)
+            .setDescription(`<a:verified:845321565339516978> | A Steam hex **${id}** Teve a Whitelist aprovada.`)
             .setColor(`292928`)
             message.channel.send(embed)
             message.delete()
