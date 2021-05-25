c8ec11716a41488ceffe24019573e71fe895b6ae
Espanholzx <77551376+Espanholzx@users.noreply.github.com>
2021-05-25 11:56:40 -0300

    Update unban.js

--- cmds/unban.js
+++ cmds/unban.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 const mysql = require("mysql")
+const {name, ip, db, password} = require('./db.json');
 
 module.exports = {
     name: "unban",
@@ -7,17 +8,17 @@ module.exports = {
     aliases: ["unban"],
     async execute(bot,message,args) {
         let con = mysql.createConnection({
-            host: "127.0.0.1", // IP DA DB
-            user: "root", // USER DA DB
-            password: "", // SENHA DA DB
-            database: "vrp" // Bota sua Db aqui '-'
+            host: ip,
+            user: name,
+            password: password,
+            database: db,
         })
 
         let id = args[0]
-        if(!id) return message.reply("Você não mencionou um ID.") // VAI RETORNAR SE NÃO BOTAR UM ID
-        if(isNaN(id)) return message.reply("Isso não é um ID válido.") // VAI RETORNAR SE NÃO FOR UM NUMERO
+        if(!id) return message.reply("Você não mencionou um ID.")
+        if(isNaN(id)) return message.reply("Isso não é um ID válido.")
         let valor = 0
-        let sql = `UPDATE vrp_users SET banned = '${valor}' WHERE id = '${id}'` // MUDA DE DB PARA DB
+        let sql = `UPDATE vrp_users SET banned = '${valor}' WHERE id = '${id}'`
         con.query(sql, function (err, result) {
             if (err) throw err;
             let embed = new Discord.MessageEmbed()
