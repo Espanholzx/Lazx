df8e17d5d6dea97d4212d07f21a760d3469dd8c2
Espanholzx <77551376+Espanholzx@users.noreply.github.com>
2021-05-25 11:56:28 -0300

    Update placa.js

--- cmds/placa.js
+++ cmds/placa.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 const mysql = require("mysql")
+const {name, ip, db, password} = require('./db.json');
 
 module.exports = {
     name: "placa",
@@ -7,23 +8,23 @@ module.exports = {
     aliases: ["placa"],
     async execute(bot,message,args) {
         let con = mysql.createConnection({
-            host: "127.0.0.1", // IP DA DB
-            user: "root", // USER DA DB
-            password: "", // SENHA DA DB
-            database: "vrp" // NOME DA DB
+            host: ip,
+            user: name,
+            password: password,
+            database: db,
         })
 
         let id = args[0]
-        if(!id) return message.reply("Você não colocou um ID Válido.") // VAI RETORNAR SE NÃO COLOCAR UM ID
-        if(isNaN(id)) return message.reply("Você não colocou um id válido, precisa ser número.") // VAI RETORNAR SE NÃO FOR UM NUMERO
-        let placa = args[1] 
-        if(!placa) return message.reply("Você não colocou uma placa.") // VAI RETORNAR SE NÃO DIGITAR NADA
-        if (placa.length > 8) return message.reply("Só pode 8 Digitos.") // VAI RETORNARA SE FOR MAIS DE 8 DIGITOS
-        let sql = `UPDATE vrp_user_identities SET registration = '${placa}' WHERE user_id = '${id}'` // MUDA DE DB PARA DB
+        if(!id) return message.reply("Você não colocou um ID Válido.")
+        if(isNaN(id)) return message.reply("Você não colocou um id válido, precisa ser número.")
+        let placa = args[1]
+        if(!placa) return message.reply("Você não colocou uma placa.")
+        if (placa.length > 8) return message.reply("Só pode 8 Digitos.")
+        let sql = `UPDATE vrp_user_identities SET registration = '${placa}' WHERE user_id = '${id}'`
         con.query(sql, function (err, result) {
             if (err) throw err;
             let embed = new Discord.MessageEmbed()
-            .setDescription(`<a:verified:845321565339516978> | O ID **${id}** Teve a placa alterada para **${placa}**.`) // VARIAVEIS ID E PLACA
+            .setDescription(`<a:verified:845321565339516978> | O ID **${id}** Teve a placa alterada para **${placa}**.`)
             .setColor(`292928`)
             message.channel.send(embed)
             message.delete()
