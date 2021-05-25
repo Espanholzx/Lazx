cc1c89e30f2c1e5932dea93dcf9579efb9e32569
Espanholzx <77551376+Espanholzx@users.noreply.github.com>
2021-05-25 11:55:35 -0300

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -1,29 +1,28 @@
 const Discord = require("discord.js")
 const mysql = require("mysql")
-
+const {name, ip, db, password} = require('./db.json');
 module.exports = {
     name: "ban",
     description: "ban",
     aliases: ["ban"],
     async execute(bot,message,args) {
         let con = mysql.createConnection({
-            host: "127.0.0.1", // Host da db
-            user: "root", // User da db
-            password: "", // Senha da db
-            database: "vrp" // Bota sua Db aqui '-'
+            host: ip,
+            user: name,
+            password: password,
+            database: db,
         })
 
         let id = args[0]
-        if(!id) return message.reply("Você não mencionou um ID.")
-        if(isNaN(id)) return message.reply("Isso não é um ID válido.")
+        if(!id) return message.reply("Você não mencionou o ID")
         let valor = 1
-        let sql = `UPDATE vrp_users SET banned = '${valor}' WHERE id = '${id}'` // Isso aqui pode mudar de db para db (Se não souber como ver isso chama Discord
+        let sql = `UPDATE vrp_users SET banned = '${valor}' WHERE id = '${id}'`
         con.query(sql, function (err, result) {
             if (err) throw err;
             let embed = new Discord.MessageEmbed()
-            .setDescription(`<a:ban:845317927938162738> | O ID **${id}** Foi Banido.`) // Se quiser alterar a mensagem a variavel é id
-            .setColor(`292928`) // Color em Hex
-            message.channel.send(embed) // Se quiser da pra botar um canal predefinido aqui ele vai mandar aonde o comando foi executado
+            .setDescription(`<a:ban:845317927938162738> | O ID **${id}** Foi Banido.`)
+            .setColor(`292928`)
+            message.channel.send(embed)
             message.delete()
         })
     }
