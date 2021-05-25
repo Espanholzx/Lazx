f26b8f9e3b931c81b1dc26d06509a92156ca7cf3
Espanholzx <77551376+Espanholzx@users.noreply.github.com>
2021-05-25 11:56:17 -0300

    Update numero.js

--- cmds/numero.js
+++ cmds/numero.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 const mysql = require("mysql")
+const {name, ip, db, password} = require('./db.json');
 
 module.exports = {
     name: "numero",
@@ -7,24 +8,24 @@ module.exports = {
     aliases: ["numero"],
     async execute(bot,message,args) {
         let con = mysql.createConnection({
-            host: "127.0.0.1", // ip da db
-            user: "root", // user da db
-            password: "", // senha da db
-            database: "vrp" // nome da db
+            host: ip,
+            user: name,
+            password: password,
+            database: db,
         })
 
         let id = args[0]
-        if(!id) return message.reply("Você não colocou o ID.") // vai retornar se n mencionar nenhum id
-        if(isNaN(id)) return message.reply("Isso não é um ID Válido, Tem que ser um número.") // vai retornar se não for um número
+        if(!id) return message.reply("Você não colocou o ID.")
+        if(isNaN(id)) return message.reply("Isso não é um ID Válido, Tem que ser um número.")
         let numero = args[1]
-        if(!numero) return message.reply("Você não colocou um numero de telefone.") // Vai retornar se não botar nada
-        if (numero.length > 6) return message.reply("Só pode 6 digitos") // vai retornar se for mais de 6 digitos
-        if(isNaN(numero)) return message.reply("Isso não é um numero Válido, precisa ser Números.") // vai retornar se não for numero
-        let sql = `UPDATE vrp_user_identities SET phone = '${numero}' WHERE user_id = '${id}'` // Isso muda de db para db
+        if(!numero) return message.reply("Você não colocou um numero de telefone.")
+        if (numero.length > 6) return message.reply("Só pode 6 digitos")
+        if(isNaN(numero)) return message.reply("Isso não é um numero Válido, precisa ser Números.")
+        let sql = `UPDATE vrp_user_identities SET phone = '${numero}' WHERE user_id = '${id}'`
         con.query(sql, function (err, result) {
             if (err) throw err;
             let embed = new Discord.MessageEmbed()
-            .setDescription(`<a:verified:845321565339516978> | O Número do ID **${id}** foi alterado para **${numero}**`) // da pra alterar as variaveis são numero e id
+            .setDescription(`<a:verified:845321565339516978> | O Número do ID **${id}** foi alterado para **${numero}**`)
             .setColor(`292928`)
             message.delete()
             message.channel.send(embed)
